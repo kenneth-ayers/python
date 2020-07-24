a08a550516832ef10bb9c438915f8af66ed64754
kenneth-ayers <46658246+kenneth-ayers@users.noreply.github.com>
2020-07-23 17:25:01 -0700

    Add files via upload

---
+++ first password system.py
@@ -0,0 +1,8 @@
+username = input("what is your username")
+password = input("what is your password")
+CorrectUser = "Kenneth"
+Pass = 123
+if username == CorrectUser & password == Pass:
+    print("hello user")
+else:
+    print ("unauthorized user please leave")
